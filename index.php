cf3d319d26e23cc3ee38cd893e2ac9193decf7f9
SavkivAlex <savkiv@gmail.com>
2015-07-13 22:37:56 +0300

    array

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-
-phpinfo();
-
+$arr = array(
+    'Ukraine' => array('Kiev', 'Lviv'),
+    'France'  => array('Paris', 'Lion'),
+    'England' => array('London', 'Liverpool'));
+foreach($arr as $name => $country)
+{
+    echo "<h1>$name: </h1>";
+    foreach($country as $town)
+    {
+        echo $town."  ";
+    }
+    echo "<br/>";
+}
 ?>
