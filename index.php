11113e394bfb9faa1a3ee529473a7c4c34019857
Icar05 <galiatkin.o@gmail.com>
2015-07-15 22:24:11 +0300

    Играюсь с гет

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
+$text = 0;
 
-echo '<a href="news.php?id="test"">Button</a>';
 
-$text = $_GET['id'];
-echo '<br>Пришло .$text<br>';
+
+echo "<a href='index.php?id=1'>Button</a>";
+$text += $_GET['id'];
+
+echo "<br>Пришло $text<br>";
 
 
 ?>
