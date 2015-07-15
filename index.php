d1ba31aaa5d5a84597efb93c45c6aa07f2271b23
Icar05 <galiatkin.o@gmail.com>
2015-07-15 22:05:16 +0300

    Играюсь с гет

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
-$arr = array(
-    'Ukraine' => array('Kiev', 'Lviv'),
-    'France'  => array('Paris', 'Lion'),
-    'England' => array('London', 'Liverpool'));
-foreach($arr as $name => $country)
-{
-    echo "<h1>$name: </h1>";
-    foreach($country as $town)
-    {
-        echo $town."  ";
-    }
-    echo "<br/>";
-}
+
+
+echo '<a href="news.php?id="test"">Button</a>';
+
+$text = $_GET['id'];
+echo '<br>Пришло .$text<br>';
+
+
 ?>
