075a3cad667e4e51964abda51e1123d707219b9a
SavkivAlex <savkiv@gmail.com>
2015-07-15 22:00:23 +0300

    Calc

--- index.php
+++ index.php
@@ -1,15 +1,52 @@
 <?php
-$arr = array(
-    'Ukraine' => array('Kiev', 'Lviv'),
-    'France'  => array('Paris', 'Lion'),
-    'England' => array('London', 'Liverpool'));
-foreach($arr as $name => $country)
+function result ($a, $b, $z)
 {
-    echo "<h1>$name: </h1>";
-    foreach($country as $town)
+    switch ($z)
+
     {
-        echo $town."  ";
+        case '+':
+        {
+            $rez = $a + $b;
+            break;
+        }
+        case '-':
+        {
+            $rez = $a - $b;
+            break;
+        }
+        case '*':
+        {
+            $rez = $a * $b;
+            break;
+        }
+        case '/':
+        {
+            if($b == 0)
+                $rez = "Error!";
+            else
+                $rez = $a + $b;
+            break;
+        }
     }
-    echo "<br/>";
+    return $rez;
 }
+
+$rez = result($_POST['a'], $_POST['b'], $_POST['z']);
+
+
 ?>
+
+<form action="index.php" method="post">
+    <input type="text" name="a" value="<?php echo $_POST['a'];?>">
+
+    <select name="z">
+        <option value="+" <?php if($_POST['z'] == '+') echo 'selected'; ?>>+</option>
+        <option value="-" <?php if($_POST['z'] == '-') echo 'selected'; ?>>-</option>
+        <option value="*" <?php if($_POST['z'] == '*') echo 'selected'; ?>>*</option>
+        <option value="/" <?php if($_POST['z'] == '/') echo 'selected'; ?>>/</option>
+    </select>
+
+    <input type="text" name="b" value="<?php echo $_POST['b'];?>">
+    <input type="submit" value="=" >
+    <input type="text" name="rez" value="<?php echo $rez;?>" disabled >
+</form>
